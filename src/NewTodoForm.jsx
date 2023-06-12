dce7f89dd1539cd4017e4255192c010b0fcceefe
Techera, Fabrizio <fabrizio.techera@oracle.com>
2023-06-11 23:56:56 -0300

    first commit

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function NewTodoForm({ onSubmit }) {
+  const [newItem, setNewItem] = useState("");
+
+  //adds a new todo to the todos array
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+
+    onSubmit(newItem);
+
+    setNewItem("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
+}
